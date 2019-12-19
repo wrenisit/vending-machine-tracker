12a2f1c3656d697c883a2ee59ff06db986a444fd
Wren Steitle <lauren.steitle@gmail.com>
2019-12-19 10:21:44 -0700

    Complete user story 3

--- spec/features/snack_show_page_spec.rb
+++ spec/features/snack_show_page_spec.rb
@@ -5,21 +5,26 @@ RSpec.describe "snack show page" do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Snacks")
     jims  = owner.machines.create(location: "Jim's Snacks")
-    item_1 = Snack.create(name: "Chex Mix", price: 2.5)
+    item_1 = Snack.create(name: "Chex Mix", price: 2)
     item_2 = Snack.create(name: "Skittles", price: 3)
-    MachineSnacks.create(machine_id: dons.id, snack_id: item_1.id)
-    MachineSnacks.create(machine_id: dons.id, snack_id: item_2.id)
-    MachineSnacks.create(machine_id: jims.id, snack_id: item_1.id)
+    MachineSnack.create(machine_id: dons.id, snack_id: item_1.id)
+    MachineSnack.create(machine_id: dons.id, snack_id: item_2.id)
+    MachineSnack.create(machine_id: jims.id, snack_id: item_1.id)
 
-    visit "snacks/#{item_2.id}"
-    expect(page).to have_content(item_2.name)
-    expect(page).to have_content(item_2.price)
+    visit "snacks/#{item_1.id}"
+    expect(page).to have_content(item_1.name)
+    expect(page).to have_content(item_1.price)
+
     within "#location-#{dons.id}" do
       expect(page).to have_content(dons.location)
-      expect(page).to have_content("Average Price: $1.50")
-      expect(page).to have_content
+      expect(page).to have_content("Average Price: $2.50")
+      expect(page).to have_content("Items in Machine: 2")
+    end
 
-    expect(page).to have_content(jims.location)
-
+    within "#location-#{jims.id}" do
+      expect(page).to have_content(jims.location)
+      expect(page).to have_content("Average Price: $2.00")
+      expect(page).to have_content("Items in Machine: 1")
+    end
   end
 end
